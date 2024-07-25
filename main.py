e3bf6240a92f757b7df46b1c49de8c0b6ec354c4
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-07-25 14:02:28 +0300

    Fix error and add simple vot`s answer logic
    Fix error: add random_id param to vk.message method

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import os
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
 
+import vk_api
 from dotenv import load_dotenv
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 load_dotenv()
 token = os.getenv("VK_SECRET_TOCKEN")
 
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
+
+
 def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message})
+    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': vk_api.utils.get_random_id()})
 
 
 for event in longpoll.listen():
-    pass
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            request = event.text
+            if request == "привет":
+                write_msg(event.user_id, "Хай")
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
+            else:
+                write_msg(event.user_id, "Не поняла вашего ответа...")
