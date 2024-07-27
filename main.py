6e7972f0d70ecc113b2621cae78a6c6b638a5afc
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-07-27 15:08:19 +0300

    Add rights control
    Only users from allowed list can activate/deactivate queue

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 from answers import *
 import vk_api
@@ -9,9 +10,11 @@ load_dotenv()
 token = os.getenv("VK_SECRET_TOCKEN")
 
 vk = vk_api.VkApi(token=token)
+api_vk = vk.get_api()
 longpoll = VkLongPoll(vk)
 
-
+admins = json.loads(os.getenv("ADMINS_SREEN_NAME_LIST"))
+print(admins)
 def write_msg(user_id: int, message: str) -> None:
     vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': vk_api.utils.get_random_id()})
 
@@ -38,17 +41,21 @@ for event in longpoll.listen():
                 answer = QueuesAnswer.get_inactive_queues_answer(content)
                 write_msg(event.user_id, answer)
             elif request[:12].lower() == "активировать" or request[:14].lower() == "деактивировать":
-                if request[:12] == "активировать":
-                    new_state = True
-                    request = request[12:]
+                if api_vk.users.get(user_ids=event.user_id, fields=["screen_name", ])[0]["screen_name"] in admins:
+                    if request[:12] == "активировать":
+                        new_state = True
+                        request = request[12:]
+                    else:
+                        new_state = False
+                        request = request[14:]
+                        
+                    if not request:
+                        write_msg(event.user_id, "Вы не указали очередь")
+                    else:
+                        queues_to_activate = list(map(str.strip, request.split(',')))
+                        answer = QueuesAnswer.get_activity_change_attempt_answer(queues_to_activate, new_state)
+                        write_msg(event.user_id, answer)
                 else:
-                    new_state = False
-                    request = request[14:]
-                if not request:
-                    write_msg(event.user_id, "Вы не указали очередь")
-                else:
-                    queues_to_activate = list(map(str.strip, request.split(',')))
-                    answer = QueuesAnswer.get_activity_change_attempt_answer(queues_to_activate, new_state)
-                    write_msg(event.user_id, answer)
+                    write_msg(event.user_id, "У вас нет прав для этого действия")
             else:
                 write_msg(event.user_id, "Не поняла вашего ответа...")
